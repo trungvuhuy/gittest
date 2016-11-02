415386f3b0a157ba6104eb0fbf849b477d494c03
ht <ht@G5>
2016-11-02 15:38:46 +0700

    modify lan 2

--- src/testgit/TestGit.java
+++ src/testgit/TestGit.java
@@ -17,6 +17,7 @@ public class TestGit {
     public static void main(String[] args) {
         // TODO code application logic here
         System.err.println("ccccc");
+        System.err.println("xxxxx");
     }
     
 }
